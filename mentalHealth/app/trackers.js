19512b5a464a8d2e4c1d58f53975af139bb557ec
Akshayyy03 <akshaymaheta3@gmail.com>
2025-03-07 14:35:31 +0530

    db integration

---
+++ mentalHealth/app/trackers.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import WalkingTrackerScreen from "../screens/WalkingTrackerScreen";
+import SleepTrackerScreen from "../screens/SleepTrackerScreen";
+import StreakTrackerScreen from "../screens/StreakScreen";
+import { View, Text } from "react-native";
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function TrackersScreen() {
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: "#4A90E2" },
+          tabBarLabelStyle: { fontSize: 14, fontWeight: "bold", color: "white" },
+          tabBarIndicatorStyle: { backgroundColor: "white", height: 3 },
+        }}
+      >
+        <Tab.Screen name="Walking" component={WalkingTrackerScreen} />
+        <Tab.Screen name="Sleep" component={SleepTrackerScreen} />
+        <Tab.Screen name="Streak" component={StreakTrackerScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
